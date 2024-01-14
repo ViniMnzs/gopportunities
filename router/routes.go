30c006c2d1d741fdda396a31ad68cfca4921c0c0
Vinicius Ribeiro Menezes <drt67706@MAC452078-m.local>
2024-01-14 16:27:26 -0300

    feat: adding routes

---
+++ router/routes.go
@@ -0,0 +1,37 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+
+	v1.GET("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v1.POST("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v1.DELETE("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v1.PUT("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	v1.GET("/openings", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+}
